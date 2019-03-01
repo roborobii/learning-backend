79c159d90c8b5e57f89720ce90e28cfe11706e1f
Robin <racristob@gmail.com>
2019-03-01 23:09:50 +0000

    API call json parse

--- APIs/first_req.js
+++ APIs/first_req.js
@@ -1,16 +1,9 @@
 var request = require('request');
 
-request('http://www.reddin3k21t.com', function(error, response, body) {
-//   if (!error && response.statusCode == 200) {
-//       console.log(body);
-//   } 
-    if (error) {
-        console.log("something went wrong");
-        console.log(error);
-    } else {
-        if (response.statusCode == 200) {
-            // things worked
-            console.log(body);
-        }
-    }
+console.log("Weather today is ...");
+request('https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22', function(error, response, body) {
+  if (!error && response.statusCode == 200) {
+      var parsedData = JSON.parse(body);
+      console.log(parsedData["weather"][0]["description"]); // body is a json but type string, not a javascript object
+  } 
 });
